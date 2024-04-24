79a75c0effc9981b72e54ccdc1607afd4b2143e3
Raven Hendricks <ravenkbeats@gmail.com>
2024-04-23 20:34:57 -0400

    edited init function

--- server/index.js
+++ server/index.js
@@ -8,8 +8,13 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const path = require("path");
 
+//acknowledge database
 const db = require("./db");
 
+//iport functions
+const {client} = db;
+
+//express app
 const app = express();
 module.exports = app;
 
@@ -17,7 +22,15 @@ module.exports = app;
 
 //init
 const init = async ()=>{
-    await Client.connect();
+
+    //connect to database
+    await client.connect();
+
+    //establish port
+    const port = process.env.port || 3000;
+
+    //start app
+    app.listen(PORT, () => console.log(`Now listening on port ${port}.`));
 }
 
 //init invocation
